a8a6d982f4fa075164fd8efc146c4ff0ad0ce403
Karol <ordon1313@gmail.com>
2015-02-14 00:03:03 +0100

    Added swiss knife

--- app/src/main/java/com/ordonteam/MainActivity.groovy
+++ app/src/main/java/com/ordonteam/MainActivity.groovy
@@ -4,22 +4,23 @@ import android.app.Activity
 import android.os.Bundle
 import android.widget.LinearLayout
 import android.widget.TextView
+import com.arasthel.swissknife.SwissKnife
+import com.arasthel.swissknife.annotations.InjectView
 import groovy.transform.CompileStatic
 
 @CompileStatic
 class MainActivity extends Activity {
+
+    @InjectView(R.id.textView)
     TextView text
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState)
-        LinearLayout linearLayout = new LinearLayout(this)
-        linearLayout.setOrientation(LinearLayout.VERTICAL)
+        setContentView(R.layout.main_activity)
 
-        text = new TextView(this)
+        SwissKnife.inject(this)
+
         text.setText('Hello Groovy')
-        linearLayout.addView(text)
-
-        setContentView(linearLayout)
     }
 }
