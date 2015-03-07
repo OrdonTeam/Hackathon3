314394b9b0674f9df22d22fb343e82dceebe2d39
Karol <ordon1313@gmail.com>
2015-03-07 13:45:31 +0100

    game layout

--- app/src/main/java/com/ordonteam/hackathon3/GameActivity.groovy
+++ app/src/main/java/com/ordonteam/hackathon3/GameActivity.groovy
@@ -1,8 +1,15 @@
 package com.ordonteam.hackathon3
 
 import android.app.Activity
+import android.os.Bundle
 import groovy.transform.CompileStatic
 
 @CompileStatic
 class GameActivity extends Activity{
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.game_activity)
+    }
 }
