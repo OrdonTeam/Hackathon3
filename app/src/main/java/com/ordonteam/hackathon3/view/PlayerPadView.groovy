3aa1479b0a7fc20d7fc8c8a730a68d0cf59f0bb0
ghost <kas_kon5@wp.pl>
2015-03-07 14:51:02 +0100

    square moved :D

---
+++ app/src/main/java/com/ordonteam/hackathon3/view/PlayerPadView.groovy
@@ -0,0 +1,22 @@
+package com.ordonteam.hackathon3.view
+
+import android.content.Context
+import android.util.AttributeSet
+import android.widget.LinearLayout
+import groovy.transform.CompileStatic
+
+@CompileStatic
+class PlayerPadView extends LinearLayout{
+    PlayerPadView(Context context) {
+        super(context)
+    }
+
+    PlayerPadView(Context context, AttributeSet attrs) {
+        super(context, attrs)
+    }
+
+    PlayerPadView(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle)
+    }
+
+}
